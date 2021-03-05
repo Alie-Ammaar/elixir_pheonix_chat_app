dc26be7ada436e709a82f34be0d9294cc3c05000
Ali Ammaar <alie.ammaar@gmail.com>
2021-03-05 13:19:25 +0500

    Use production config from Heroku ENV variables

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,17 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :elixir_pheonix_chat, ElixirPheonixChatWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "elphxchat.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+# Database configs
+config :elixir_pheonix_chat, ElixirPheonixChat.Repo,
+adapter: Ecto.Adapters.Postgres,
+url: System.get_env("DATABASE_URL"),
+pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -52,4 +61,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
